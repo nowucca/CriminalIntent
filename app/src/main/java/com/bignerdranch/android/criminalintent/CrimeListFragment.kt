b78870a1b560f49313d07a52d8a9736052f5e4ca
nowucca <steven.atkinson@GMAIL.COM>
2020-02-18 00:12:53 -0800

    Links recycler view with adapter and models

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -16,6 +16,7 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
 
   private lateinit var crimeRecyclerView: RecyclerView
+  private var adapter: CrimeAdapter? = null
 
   private val crimeListViewModel: CrimeListViewModel by lazy {
     ViewModelProvider(this).get(CrimeListViewModel::class.java)
@@ -37,7 +38,16 @@ class CrimeListFragment : Fragment() {
       view.findViewById(R.id.crime_recycler_view) as RecyclerView
     crimeRecyclerView.layoutManager = LinearLayoutManager(context)
 
+    updateUI()
+
     return view
+  }
+
+
+  private fun updateUI() {
+    val crimes = crimeListViewModel.crimes
+    adapter = CrimeAdapter(crimes)
+    crimeRecyclerView.adapter = adapter
   }
 
   private inner class CrimeHolder(view: View)

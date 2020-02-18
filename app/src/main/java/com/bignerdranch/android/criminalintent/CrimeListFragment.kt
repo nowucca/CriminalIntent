fc6b34e45f258dad63a906aa49fc8aa198437287
nowucca <steven.atkinson@GMAIL.COM>
2020-02-18 00:19:21 -0800

    Removes crime knowledge from adapter

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -53,8 +53,16 @@ class CrimeListFragment : Fragment() {
   private inner class CrimeHolder(view: View)
     : RecyclerView.ViewHolder(view) {
 
+    private lateinit var crime: Crime
+
     val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
     val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+
+    fun bind(crime: Crime) {
+      this.crime = crime
+      titleTextView.text = this.crime.title
+      dateTextView.text = this.crime.date.toString()
+    }
   }
 
   private inner class CrimeAdapter(var crimes: List<Crime>)
@@ -70,10 +78,7 @@ class CrimeListFragment : Fragment() {
 
     override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
       val crime = crimes[position]
-      holder.apply {
-        titleTextView.text = crime.title
-        dateTextView.text = crime.date.toString()
-      }
+      holder.bind(crime)
     }
   }
 

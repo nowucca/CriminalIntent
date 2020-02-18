d7351b1c6ce4045ae72667d5583b4eeab8d8fd66
nowucca <steven.atkinson@GMAIL.COM>
2020-02-18 00:10:05 -0800

    Adds crime holder

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -38,6 +39,14 @@ class CrimeListFragment : Fragment() {
 
     return view
   }
+
+  private inner class CrimeHolder(view: View)
+    : RecyclerView.ViewHolder(view) {
+
+    val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
+    val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
+  }
+
   companion object {
     fun newInstance(): CrimeListFragment {
       return CrimeListFragment()
